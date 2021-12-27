c339af73bb5af85a25d2563b7f2f47915621404a
Hexhile <alantobarg@gmail.com>
2021-12-27 15:20:01 -0600

    repair

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'polls.apps.PollsConfig',
-    'blog.apps.BlogConfig',
+    #'polls.apps.PollsConfig',
+    #'blog.apps.BlogConfig',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
