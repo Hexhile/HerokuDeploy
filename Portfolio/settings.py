6f7d6579ba7696cb366566c071128ae3b85b5433
Hexhile <alantobarg@gmail.com>
2021-12-27 15:37:12 -0600

    repair

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'portfolio',
         'USER': 'hexhile',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
